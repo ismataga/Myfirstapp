e0ab337a23115b61d8616a33e88d362c89f2cd85
Ismat <ismataga1@gmail.com>
2023-04-06 11:01:11 +0400

    Practice

---
+++ src/udemyCourse1/Tapsiriqlar/tasks/Task11/T2.java
@@ -0,0 +1,14 @@
+//package udemyCourse1.Tapsiriqlar.tasks.Task11;
+//
+//public class T2 {
+//    public static void main(String[] args) {
+//        int[] arr = {1,2,3,4,5};
+//
+//        while (arr<=5){
+//            char a = ' ';
+//
+//
+//
+//        }
+//    }
+//}
